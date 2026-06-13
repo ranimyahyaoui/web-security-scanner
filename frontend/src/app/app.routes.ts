f159b75e89f58849d48d560610218d3a4ecafb58
ranimyahyaoui <ranim.yahyaoui@supcom.ucar.tn>
2026-06-13 02:34:25 +0100

    fixer register

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,12 +5,15 @@ import { History } from './pages/history/history';
 import { LoginComponent } from './pages/login/login';
 import { authGuard } from './guards/auth-guard';
 import { NotFound } from './pages/not-found/not-found';
+import { RegisterComponent } from './pages/register/register';
 
 export const routes: Routes = [
   { path: 'dashboard', canActivate: [authGuard], component: Dashboard  },
   { path: 'scan',canActivate: [authGuard], component: Scan },
   { path: 'history',canActivate: [authGuard] ,component: History },
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent },
+
+  { path: '', redirectTo: 'register', pathMatch: 'full' },
   { path: '**', component: NotFound }
 ];
