e76ab7db66ace59a3eb75d0044ebf23d0062e75f
ranimyahyaoui <ranim.yahyaoui@supcom.ucar.tn>
2026-06-13 02:41:42 +0100

    fix: cors

--- backend/server.js
+++ backend/server.js
@@ -14,18 +14,7 @@ const scanRoutes=require('./routes/scanRoutes');
 const { scanWebsite } = require('./services/scannerService');
 const app=express();
 app.use(express.json());
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'La politique CORS de ce site ne permet pas l\'accès depuis l\'origine spécifiée.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true 
-}));
+app.use(cors());
 app.use("/api/auth",authRoutes);
 app.use("/api/scans",scanRoutes);
 const PORT=process.env.PORT || 5000;
