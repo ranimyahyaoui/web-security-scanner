7f57d166a2f13bae055606e05867eb6cd4b38c07
ranimyahyaoui <ranim.yahyaoui@supcom.ucar.tn>
2026-06-13 01:23:40 +0100

    fixer cors car j'ai autorise a tous les frontends de communiquer avec backend maintenant je precise

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,31 @@
 require('dotenv').config();
 const express=require('express');
-const cors=require('cors');
+const cors = require('cors');
+
+const allowedOrigins = [
+  'http://localhost:4200', 
+  'https://votre-projet-front.vercel.app' 
+];
+
+
 require('./config/connect');
 const authRoutes=require('./routes/authRoutes');
 const scanRoutes=require('./routes/scanRoutes');
 const { scanWebsite } = require('./services/scannerService');
 const app=express();
-app.use(cors());
 app.use(express.json());
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'La politique CORS de ce site ne permet pas l\'accès depuis l\'origine spécifiée.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true 
+}));
 app.use("/api/auth",authRoutes);
 app.use("/api/scans",scanRoutes);
 const PORT=process.env.PORT || 5000;
